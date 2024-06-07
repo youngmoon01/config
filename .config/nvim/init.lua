4ddc8ff323d27e7a4d97a82f95db5be2957e4724
youngmoon01 <eomyoungmoon01@gmail.com>
2024-06-07 03:28:42 +0000

    Add pakcer itself to packer managed packages list

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.cmd("source " .. vimrc)
 
 local use = require('packer').use
 require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
