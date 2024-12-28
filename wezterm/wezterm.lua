7ffb1dd375881af41d7217da40659f6e8db5e94a
Youngmoon Eom <eomyoungmoon01@gmail.com>
2024-12-28 12:26:43 +0900

    update weezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,10 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('JetBrains Mono')
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = 'Seafoam Pastel'
 config.color_scheme = 'Sonokai (Gogh)'
 config.window_background_opacity = 0.9
 
--- and finally, return the configuration to wezterm
 return config
