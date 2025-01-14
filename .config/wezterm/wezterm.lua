b951c43e6ca05987f58426207ef5aa664288669d
Youngmoon Eom <eomyoungmoon01@gmail.com>
2025-01-14 23:30:21 +0900

    chore: update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font("JetBrains Mono")
 
-config.color_scheme = 'Sonokai (Gogh)'
-config.window_background_opacity = 0.9
+config.color_scheme = "OneDark (base16)"
 
 return config
